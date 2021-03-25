f282832f83c10f9ad3c4cc4df501a22816c82992
higgs508 <79808324+higgs508@users.noreply.github.com>
2021-03-24 21:35:31 -0500

    took out compute function

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-function compute()
-{
-    var principal = document.getElementById("principal");
-	var rate = document.getElementById("rate");
-	var years = document.getElementById("years");
-	var interest = (principal*years*rate/100);
-	var date = currentTime.getFullYear();
-	var future = date + years;
-}
-
 var rangeslider = document.getElementById("rate");
 var output = document.getElementById("intrate");
 output.innerHTML = rangeslider.value;
