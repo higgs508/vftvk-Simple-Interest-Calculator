4a30a691e368503cbbab1a3e3b26f4125c4d0b58
higgs508 <79808324+higgs508@users.noreply.github.com>
2021-03-24 20:10:56 -0500

    onchange to oninput

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var rangeSlider = document.getElementById("rate");
 var output = document.getElementById("intRate");
 output.innerHTML = rangeSlider.value;
 
-rangeslider.onchange = function(){
+rangeslider.oninput = function(){
 		output.innerHTML = this.value;
 }
         
