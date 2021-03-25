a24b1b23f4e428be414ac2cf91b9df5e79b5462b
higgs508 <79808324+higgs508@users.noreply.github.com>
2021-03-24 21:42:48 -0500

    put function compute back

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+function compute()
+{
+    var principal = document.getElementById("principal");
+	var rate = document.getElementById("rate");
+	var years = document.getElementById("years");
+	var interest = (principal*years*rate/100);
+	var date = currentTime.getFullYear();
+	var future = date + years;
+}
+
 var rangeslider = document.getElementById("rate");
 var output = document.getElementById("intrate");
 output.innerHTML = rangeslider.value;
