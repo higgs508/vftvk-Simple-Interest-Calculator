fa5675052b36d5e4844c351d27cca015cca19a6e
higgs508 <79808324+higgs508@users.noreply.github.com>
2021-03-26 20:31:35 -0500

    testing what was in the HTML document exactly

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
-function compute()
-{
-    var principal = document.getElementById("principal");
-	var rate = document.getElementById("rate");
-	var years = document.getElementById("years");
-	var interest = (principal*years*rate/100);
-	var date = currentTime.getFullYear();
-	var future = date + years;
-}
-
 var rangeslider = document.getElementById("rate");
-var output = document.getElementById("intrate");
+var output = document.getElementById("intRate");
 output.innerHTML = rangeslider.value;
 
-rangeslider.onchange = function()
-{
-	output.innerHTML = rangeslider.value;
+rangeslider.onchange = function() {
+  output.innerHTML = this.value;
 }
-        
