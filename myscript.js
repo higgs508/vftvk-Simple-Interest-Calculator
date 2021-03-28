70b37929555741b2484323195ba7fcd48e70933f
higgs508 <79808324+higgs508@users.noreply.github.com>
2021-03-27 20:30:10 -0500

    goofin

--- myscript.js
+++ myscript.js
@@ -1,7 +1,10 @@
+$(document).ready(function(){
+
 var rangeslider = document.getElementById("rate");
-var output = document.getElementById("intRate");
+var output = document.getElementById("intrate");
 output.innerHTML = rangeslider.value;
 
 rangeslider.onchange = function() {
   output.innerHTML = this.value;
 }
+});
