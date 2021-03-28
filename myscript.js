ee4bbc2b64a3a6af88b156e48f470d762fb201fb
higgs508 <79808324+higgs508@users.noreply.github.com>
2021-03-27 20:43:16 -0500

    undoskis

--- myscript.js
+++ myscript.js
@@ -1,5 +1,3 @@
-$(document).ready(function(){
-
 var rangeslider = document.getElementById("rate");
 var output = document.getElementById("intrate");
 output.innerHTML = rangeslider.value;
@@ -7,4 +5,3 @@ output.innerHTML = rangeslider.value;
 rangeslider.onchange = function() {
   output.innerHTML = this.value;
 }
-});
