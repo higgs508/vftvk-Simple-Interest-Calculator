74a99b53ab4d0218975dc16c48f82bf4f1a15cb3
higgs508 <79808324+higgs508@users.noreply.github.com>
2021-07-11 22:30:46 -0500

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -21,4 +21,7 @@ function updateRate()
 }
 
 //Get reference to the element named 'result'
-document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+document.getElementById("result").innerHTML="If you deposit "+principal+",\
+<br\>at an interest rate of "+rate+"%\
+<br\>You will receive an amount of "+amount+",\
+<br\>in the year "+year+"\<br\>"
