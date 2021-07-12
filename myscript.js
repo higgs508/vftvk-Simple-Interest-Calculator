4bbf8cc2e0396ed06481170b9bb2cca8c26b902a
higgs508 <79808324+higgs508@users.noreply.github.com>
2021-07-11 22:04:54 -0500

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -2,6 +2,7 @@ var principal = document.getElementById("principal").value;
 var rate = document.getElementById("rate").value;
 var years = document.getElementById("years").value;
 var interest = principal * years * rate /100;
+var year = new Date().getFullYear()+parseInt(years);
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
