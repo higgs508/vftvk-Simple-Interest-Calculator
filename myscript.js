b02c30be04092f236a0348ba39c34480757f1619
higgs508 <79808324+higgs508@users.noreply.github.com>
2021-07-15 21:16:39 -0500

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -31,9 +31,9 @@ function compute()
 
 
 
-    document.getElementById("result").innerHTML="If you deposit "+p+
-        ",\<br\>at an interest rate of "+r+
-        "%\<br\>You will receive an amount of "+interest+
-        ",\<br\>in the year "+year+
+    document.getElementById("result").innerHTML="If you deposit "<mark>+p+</mark>
+        ",\<br\>at an interest rate of "<mark>+r+</mark>
+        "%\<br\>You will receive an amount of "<mark>+interest+</mark>
+        ",\<br\>in the year "<mark>+year+</mark>
         "\<br\>"
 }
